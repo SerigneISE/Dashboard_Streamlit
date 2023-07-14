7f80033cc12839b2433fd9994b85f49312204649
SerigneISE <115032898+SerigneISE@users.noreply.github.com>
2023-07-14 17:42:11 +0100

    Update dashboard_tuto.py

--- dashboard_tuto.py
+++ dashboard_tuto.py
@@ -14,30 +14,35 @@ achats = pd.read_csv('achats.csv')
 imp_clic = pd.merge(impressions, clics, on='cookie_id')
 imp_clic_achat = pd.merge(imp_clic, achats, on='cookie_id')
 
+# Filtrer les données en fonction des options sélectionnées
+dept_filter = st.multiselect("Sélectionner les départements", imp_clic_achat['dept'].unique())
+filtered_data = imp_clic_achat[imp_clic_achat['dept'].isin(dept_filter)]
+
 # Calcul du chiffre d'affaires
-ca = imp_clic_achat['price'].sum()
+ca = filtered_data['price'].sum()
 st.write(f"<span style='color:green; font-size:60px;'> Chiffre d'affaires : {ca} € </span>", unsafe_allow_html=True)
 
 # Age en fonction des produits
 st.subheader('Age en fonction des produits')
-box = px.box(imp_clic_achat, x='product_id', y='age')
+box = px.box(filtered_data, x='product_id', y='age')
 st.plotly_chart(box)
 
 # Répartition des ventes en fonction du sexe
 st.subheader('Répartition des ventes en fonction du sexe')
 colors = ['#FF0000', '#00FF00']  # Exemple de couleurs personnalisées (rouge et vert)
-pie = px.pie(imp_clic_achat, values='price', names='gender', color_discrete_sequence=colors)
+pie = px.pie(filtered_data, values='price', names='gender', color_discrete_sequence=colors)
 st.plotly_chart(pie)
 
 # Distribution des ventes par département
 st.subheader('Distribution des ventes par département')
-bar = px.bar(imp_clic_achat, x='dept', y='price', title='Distribution des ventes par département')
+bar = px.bar(filtered_data, x='dept', y='price', title='Distribution des ventes par département')
 st.plotly_chart(bar)
 
 # Répartition des ventes par campagne et par sexe
 st.subheader('Répartition des ventes par campagne et par sexe')
-stacked_bar = px.bar(imp_clic_achat, x='campaign_id', y='price', color='gender', barmode='stack', title='Répartition des ventes par campagne et par sexe')
+stacked_bar = px.bar(filtered_data, x='campaign_id', y='price', color='gender', barmode='stack', title='Répartition des ventes par campagne et par sexe')
 st.plotly_chart(stacked_bar)
+
 
 if __name__ == '__dashboard_tuto__':
     app.run(debug=True)
