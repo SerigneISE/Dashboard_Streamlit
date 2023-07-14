83972079c7b65d2d92077eb44e1dd29cc488fcb9
SerigneISE <115032898+SerigneISE@users.noreply.github.com>
2023-07-14 17:40:31 +0100

    Update dashboard_tuto.py

--- dashboard_tuto.py
+++ dashboard_tuto.py
@@ -18,28 +18,23 @@ imp_clic_achat = pd.merge(imp_clic, achats, on='cookie_id')
 ca = imp_clic_achat['price'].sum()
 st.write(f"<span style='color:green; font-size:60px;'> Chiffre d'affaires : {ca} € </span>", unsafe_allow_html=True)
 
-# Graphique : Vente en fonction des campagnes
-st.subheader('Vente en fonction des campagnes')
-hist = px.histogram(imp_clic_achat, x='campaign_id', y='price')
-st.plotly_chart(hist)
-
-# Graphique : Age en fonction des produits
+# Age en fonction des produits
 st.subheader('Age en fonction des produits')
 box = px.box(imp_clic_achat, x='product_id', y='age')
 st.plotly_chart(box)
 
-# Graphique : Répartition des ventes en fonction du sexe
+# Répartition des ventes en fonction du sexe
 st.subheader('Répartition des ventes en fonction du sexe')
 colors = ['#FF0000', '#00FF00']  # Exemple de couleurs personnalisées (rouge et vert)
 pie = px.pie(imp_clic_achat, values='price', names='gender', color_discrete_sequence=colors)
 st.plotly_chart(pie)
 
-# Graphique : Distribution des ventes par département
+# Distribution des ventes par département
 st.subheader('Distribution des ventes par département')
 bar = px.bar(imp_clic_achat, x='dept', y='price', title='Distribution des ventes par département')
 st.plotly_chart(bar)
 
-# Graphique : Répartition des ventes par campagne et par sexe
+# Répartition des ventes par campagne et par sexe
 st.subheader('Répartition des ventes par campagne et par sexe')
 stacked_bar = px.bar(imp_clic_achat, x='campaign_id', y='price', color='gender', barmode='stack', title='Répartition des ventes par campagne et par sexe')
 st.plotly_chart(stacked_bar)
